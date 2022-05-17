651b330999ed81cabee5cb453af542f8131594df
Developer-Rajesh <rajeshhabra100@gmail.com>
2022-05-18 02:33:29 +0530

    Google Authentication Done

--- src/Firebase.Init.js
+++ src/Firebase.Init.js
@@ -5,13 +5,21 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+// };
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyAS2nNQo-bSjLcaRUX22PchOSIX8oXq1zA",
+    authDomain: "doctors-portal-36af8.firebaseapp.com",
+    projectId: "doctors-portal-36af8",
+    storageBucket: "doctors-portal-36af8.appspot.com",
+    messagingSenderId: "863225230360",
+    appId: "1:863225230360:web:2850c1b3a4a817160aff6d",
 };
 
 // Initialize Firebase
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,64 @@
 import React from "react";
+import auth from "../../Firebase.Init";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  return (
-    <div>
-      <h1>This is Login...</h1>
-    </div>
-  );
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm();
+    if (user) {
+        console.log(user);
+    }
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="card-title justify-center">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="Your Email"
+                                class="input input-bordered w-full max-w-xs"
+                                {...register("email", {
+                                    pattern: /[A-Za-z]{3}/,
+                                    massage: "error massage",
+                                })}
+                            />
+                            <label class="label">
+                                <span class="label-text-alt">Alt label</span>
+                            </label>
+                        </div>
+                        <input />
+                        {errors.firstName?.type === "required" &&
+                            "First name is required"}
+
+                        <input {...register("lastName", { required: true })} />
+                        {errors.lastName && "Last name is required"}
+
+                        <input type="submit" />
+                    </form>
+                    <div class="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        class="btn btn-outline hover:btn hover:btn-primary hover:uppercase hover:text-white hover:font-bold hover:bg-gradient-to-r hover:from-secondary hover:to-primary"
+                    >
+                        CONTINUE WITH GOOGLE
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
